fdc256ff30367b386255e1440487a4e179e37b22
vorapon12 <112742475+vorapon12@users.noreply.github.com>
2025-10-16 19:41:53 +0700

    :sparkles: feat: schema with Category and ProductType enhancements

---
+++ src/products/category.resolver.ts
@@ -0,0 +1,53 @@
+import {
+  Resolver,
+  Query,
+  Mutation,
+  Args,
+  ID,
+  ResolveField,
+  Parent,
+} from '@nestjs/graphql';
+import { CategoryService } from './category.service';
+import { CategoryModel } from './entities/category.entity';
+import { ProductTypeModel } from './entities/product-type.entity';
+import { Category } from '@/database/entities/category.entity';
+import { ProductType } from '@/database/entities/product-type.entity';
+import { Loader } from '@tracworx/nestjs-dataloader';
+import DataLoader from 'dataloader';
+import { ProductTypeByIdLoader } from './dataloaders/product-type-by-id.loader';
+import { CreateCategoryInput } from './dto/create-category.input';
+import { CurrentUser } from '@/shared/decorators/decorators';
+
+@Resolver(() => CategoryModel)
+export class CategoryResolver {
+  constructor(private readonly categoryService: CategoryService) {}
+
+  @Query(() => [CategoryModel], { name: 'categorys' })
+  findAll(
+    @Args('productTypeId', { type: () => ID, nullable: true })
+    productTypeId?: string,
+  ) {
+    return this.categoryService.findAll(productTypeId);
+  }
+
+  @Query(() => CategoryModel, { name: 'category' })
+  findOne(@Args('id', { type: () => ID }) id: string) {
+    return this.categoryService.findOne(id);
+  }
+
+  @Mutation(() => CategoryModel, { name: 'createCategory' })
+  createCategory(
+    @Args('createCategoryInput') createCategoryInput: CreateCategoryInput,
+    @CurrentUser() user: any, // <-- นำ User ID เข้ามา
+  ) {
+    return this.categoryService.create(createCategoryInput, user.id);
+  }
+
+  @ResolveField('productType', () => ProductTypeModel)
+  getProductType(
+    @Parent() category: Category,
+    @Loader(ProductTypeByIdLoader) loader: DataLoader<string, ProductType>,
+  ) {
+    return loader.load(category.productTypeId);
+  }
+}
